f18889ba85eca0eecd1c0fa85ecd64b38369a24d
Jonathan Song <jonats10@uci.edu>
2023-11-15 16:45:33 -0800

    add date logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import "./css/styles.css";
 import { Coin } from "./coin.js";
 alert("hello");
 
-async function getCoin(coin) {
+async function getCoin(coin, date) {
   const response = await Coin.getCoin(coin);
-  const response2 = await Coin.getCoinData(coin);
+  const response2 = await Coin.getCoinData(coin,date);
 
   if (response.error) {
     printError(response, coin);
@@ -15,7 +15,7 @@ async function getCoin(coin) {
   if (response2.error) {
     printError2(response2, coin);
   } else {
-    printElement2(response2, coin);
+    printElement2(response2, coin, date);
   }
 }
 
@@ -25,8 +25,8 @@ function printElement(response, coin) {
 function printError(error, coin) {
   document.querySelector("#result").innerText = `Error ${coin}:${error}.`;
 }
-function printElement2(response2, coin) {
-  document.querySelector("#resultData").innerText = `Price of ${coin} is ${response2.market_data.current_price.usd}.`;
+function printElement2(response2, coin, date) {
+  document.querySelector("#resultData").innerText = `Price of ${coin} is ${response2.market_data.current_price.usd} on this ${date}.`;
   
 }
 function printError2(error, coin) {
@@ -37,7 +37,8 @@ function printError2(error, coin) {
 function handleSubmit(e) {
   e.preventDefault();
   const coin = document.querySelector("#coins").value;
-  getCoin(coin);
+  const date = document.querySelector("#date").value;
+  getCoin(coin,date);
 }
 
 window.addEventListener("load", () => {
